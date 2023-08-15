32b4027fca2b97f96532f1172cac285d8fef2599
lennox charles <lennyaiko17@gmail.com>
2023-08-16 00:17:31 +0100

    feat: skeleton ready

--- index.js
+++ index.js
@@ -6,17 +6,39 @@ import { Octokit } from '@octokit/rest'
 intro(color.inverse('todo-list CLI'))
 
 const repo_name = await text({
-    message: 'Enter name of the repo',
+    message: 'What is the name of the repo?',
     placeholder: 'clacky'
+})
+
+const selection = await select({
+    message: 'Do you want to view issues or PR?',
+    options: [
+        {value: 'is', label: 'issues', hint: 'to contribute'},
+        {value: 'pr', label: 'pull requests'}
+    ]
+})
+
+const type = await select({
+    message: 'Do you want open or closed?',
+    options: [
+        {value: 'open', label: 'open'},
+        {value: 'closed', label: 'closed'}
+    ]
+})
+
+const page_number = await text({
+    message: 'What page do you want to view?',
+    placeholder: '>= 1'
+})
+
+const per_page = await text({
+    message: 'How many per page?',
+    placeholder: '< 100'
 })
 
 const s = spinner()
 
-s.start("Loading...")
-s.stop("Done!...")
-
-// const choice = await confirm({
-//     message: 'Are you done?'
-// })
+s.start(`Fetching...`)
+s.stop(`Done fetching...`)
 
 outro('You are all set')
